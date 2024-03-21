35b2de496b518b85e1fe2c70bab42e8a608c54d3
Roy Shilkrot <roy.shil@gmail.com>
2024-03-21 17:30:49 -0400

    Add CUDA support for Windows builds

--- build_ct2_windows.ps1
+++ build_ct2_windows.ps1
@@ -1,4 +1,10 @@
-Param($Configuration)
+Param(
+    $Configuration,
+    $CudaVersionOrCpu
+)
+
+# stop on error
+$ErrorActionPreference = "Stop"
 
 $Version = "4.1.1"
 
@@ -16,29 +22,39 @@ Expand-Archive OpenBLAS-$OpenBLASVersion-x64.zip -DestinationPath OpenBLAS-$Open
 Remove-Item OpenBLAS-$OpenBLASVersion-x64.zip
 
 if ($Configuration -eq "Release") {
-  $runtimeFlag = "-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL"
+  $extraFlag = "-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL"
 } else {
-  $runtimeFlag = "-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebugDLL"
+  $extraFlag = "-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebugDLL"
 }
 
-cmake . -B build_$Configuration `
-  -DBUILD_SHARED_LIBS=ON `
-  -DOPENMP_RUNTIME=COMP `
-  -DWITH_CUDA=OFF `
-  -DWITH_MKL=OFF `
-  -DWITH_TESTS=OFF `
-  -DWITH_EXAMPLES=OFF `
-  -DWITH_TFLITE=OFF `
-  -DWITH_TRT=OFF `
-  -DWITH_PYTHON=OFF `
-  -DWITH_SERVER=OFF `
-  -DWITH_COVERAGE=OFF `
-  -DWITH_PROFILING=OFF `
-  -DBUILD_CLI=OFF `
-  -DWITH_OPENBLAS=ON `
-  -DOPENBLAS_INCLUDE_DIR="OpenBLAS-$OpenBLASVersion-x64\include" `
-  -DOPENBLAS_LIBRARY="OpenBLAS-$OpenBLASVersion-x64\lib\libopenblas.dll.a" `
-  $runtimeFlag
+# if CUDA_PATH exists on env variables, then build with CUDA
+$cudaBuild = [System.Environment]::GetEnvironmentVariable("CUDA_PATH", "Machine")
+if ($cudaBuild -ne $null) {
+  $cudaPathUnix = $cudaBuild -replace '\\', '/'
+  $cudaFlag = " -DWITH_CUDA=ON -DCUDA_TOOLKIT_ROOT_DIR=`"$cudaPathUnix`""
+} else {
+  $cudaFlag = "-DWITH_CUDA=OFF"
+}
+
+$command = "cmake . -B build_$Configuration " +
+    "-DBUILD_SHARED_LIBS=ON " +
+    "-DOPENMP_RUNTIME=COMP " +
+    "-DWITH_MKL=OFF " +
+    "-DWITH_EXAMPLES=OFF " +
+    "-DWITH_TFLITE=OFF " + 
+    "-DWITH_TRT=OFF " + 
+    "-DWITH_PYTHON=OFF " + 
+    "-DWITH_SERVER=OFF " + 
+    "-DWITH_COVERAGE=OFF " + 
+    "-DWITH_PROFILING=OFF " +
+    "-DBUILD_CLI=OFF " +
+    "-DWITH_OPENBLAS=ON " +
+    "-DOPENBLAS_INCLUDE_DIR=`"OpenBLAS-$OpenBLASVersion-x64\include`" " +
+    "-DOPENBLAS_LIBRARY=`"OpenBLAS-$OpenBLASVersion-x64\lib\libopenblas.dll.a`" " +
+    "$extraFlag  $cudaFlag"
+
+Write-Host $command
+Invoke-Expression $command
 
 cmake --build build_$Configuration --config $Configuration
 
@@ -47,8 +63,18 @@ New-Item -ItemType Directory -Force -Path "..\dist\"
 cmake --install build_$Configuration --config $Configuration --prefix "..\dist\$Configuration"
 # copy openblas .dll to dist folder, first create the folder
 New-Item -ItemType Directory -Force -Path "..\dist\$Configuration\bin"
-Copy-Item "OpenBLAS-$OpenBLASVersion-x64\bin\libopenblas.dll" "..\dist\$Configuration\bin\libopenblas.dll"
-Compress-Archive "..\dist\$Configuration\*" "..\dist\libctranslate2-windows-$Version-$Configuration.zip" -Verbose
+Copy-Item -Force "OpenBLAS-$OpenBLASVersion-x64\bin\libopenblas.dll" "..\dist\$Configuration\bin\libopenblas.dll"
+
+$cudaConfig = "-cpu"
+
+# copy the cublas dll if this is a cuda build
+if ($cudaBuild -ne $null) {
+  Copy-Item -Force "$cudaBuild\bin\cublas*.dll" -Destination "..\dist\$Configuration\bin\"
+  $cudaConfig = "-cuda$CudaVersionOrCpu"
+}
+
+Remove-Item -Force "..\dist\libctranslate2-windows-$Version-$Configuration$cudaConfig.zip" -ErrorAction SilentlyContinue
+Compress-Archive "..\dist\$Configuration\*" "..\dist\libctranslate2-windows-$Version-$Configuration$cudaConfig.zip" -Verbose
 
 Set-Location "..\"
 Remove-Item "CTranslate2-$Version" -Recurse -Force
